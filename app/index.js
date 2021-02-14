0a9d2696ce19721457af09ac20c0953d5ce6062a
rohit dhore <rohit.dhore1612@gmail.com>
2021-02-14 18:19:41 +0530

    toto

--- app/index.js
+++ app/index.js
@@ -1,11 +1,13 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+import TodoApp from './components/TodoApp'
 class App extends React.Component{
     render(){
         return(
-            <div>Hello World</div>
+            <TodoApp />
         )
     }
 }
