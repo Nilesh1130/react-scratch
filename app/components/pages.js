16ef586aada3bfe77500827dbcb0291fe4ca1a9d
rohit dhore <rohit.dhore1612@gmail.com>
2021-02-15 21:20:29 +0530

    added router

---
+++ app/components/pages.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export const Home = () => {
+  return <h2>Home</h2>;
+}
+
+export const About = () =>  {
+  return <h2>About</h2>;
+}
+
+export const Users = () =>  {
+  return <h2>Users</h2>;
+}
--- app/index.js
+++ app/index.js
@@ -2,12 +2,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+
+import {About, Users , Home} from './components/pages'
 
 import TodoApp from './components/TodoApp'
 class App extends React.Component{
     render(){
         return(
-            <TodoApp />
+          <Router>
+            <div>
+              <nav>
+                <ul>
+                  <li>
+                    <Link to="/high">High Priority</Link>
+                  </li>
+                  <li>
+                    <Link to="/medium">Medium Priority</Link>
+                  </li>
+                  <li>
+                    <Link to="/low">Low Priority</Link>
+                  </li>
+                </ul>
+              </nav>
+
+              {/* A <Switch> looks through its children <Route>s and
+                  renders the first one that matches the current URL. */}
+              <Switch>
+                <Route path="/low">
+                  <TodoApp title={'Low Priority'} cacheList={'low'}/>
+                </Route>
+                <Route path="/medium">
+                  <TodoApp title={'Medium Priority'} cacheList={'medium'}/>
+                </Route>
+                <Route path="/high">
+                  <TodoApp title={'High Priority'} cacheList={'high'}/>
+                </Route>
+                <Redirect from="/" to="/high" />
+              </Switch>
+            </div>
+          </Router>
         )
     }
 }
